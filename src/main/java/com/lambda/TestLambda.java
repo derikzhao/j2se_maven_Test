677086db194ea2b95555871d1b9aa58e0a12e35c
deirkzhao <245761986@qq.com>
2017-10-16 17:09:31 +0800

    test

--- src/main/java/com/lambda/TestLambda.java
+++ src/main/java/com/lambda/TestLambda.java
@@ -1,7 +1,28 @@
 package com.lambda;
 
+import com.charactor.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/10/13.
  */
 public class TestLambda {
+    public static void main(String[] args) {
+
+    }
+
+    public static void testLambda() {
+
+    }
+
+    public static void testAggregate() {
+        List<Hero> heros = new ArrayList<Hero>();
+        for (int i = 0; i < 50000; i++) {
+            heros.add(new Hero("hero " + i));
+        }
+        //heros.stream().filter(h -> h.hp > 0);
+
+    }
 }
